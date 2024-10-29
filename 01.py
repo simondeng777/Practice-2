ef20c92e6233019927443011d499e33a12aa74a7
鄧維兆 <164477084+simondeng777@users.noreply.github.com>
2024-10-29 13:08:01 +0800

    改變指令

--- 01.py
+++ 01.py
@@ -8,20 +8,23 @@ print(image.mode,image.format)
 #旋轉
 Rotate_img=image.rotate(330)
 Rotate_img.save('RT.jpg')
-display(Rotate_img)
+Image._show(Rotate_img)
 
 #模糊化
 Blur_img=image.filter(ImageFilter.GaussianBlur(radius=5))
 Blur_img.save('BU.jpg')
+Image._show(Blur_img)
 
 #銳化
 Sarp_img=image.filter(ImageFilter.UnsharpMask(radius=5))
 Sarp_img.save('SP.jpg')
+Image._show(Sarp_img)
 
 #拉伸
 W_img=image.resize((int(image.size[0]), int(image.size[1]*0.65)))
 W_img.save('W.jpg')
+Image._show(W_img)
 
 H_img=image.resize((int(image.size[0]*0.65), int(image.size[1])))
 H_img.save('H.jpg')
-
+Image._show(H_img)
